77a6e110b5bd6acf4ba262c868b0c0bbdc01fe34
Neeraj <60258936+neerajvkn@users.noreply.github.com>
2021-08-17 17:22:15 +0300

    created func to update scheduler on aws

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/api_call', methods=['POST'])
+def api_call(req_body):
+
+    api_url = 'https://8nofgv3pg0.execute-api.us-east-2.amazonaws.com/default/EC2Scheduler'
+
+    req_body = json.dumps(req_body)
+    req = requests.post(api_url, data = req_body)
+    decoded_data = req.content.decode("utf-8") 
+    return decoded_data
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
